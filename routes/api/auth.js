005f44fdd187b4f890e867b2bde9bafa13437204
Vaishnavi Ramesh <vaishnavi.ur777@gmail.com>
2020-04-06 21:54:38 +0530

    Added models and routes for user registration and authentication

---
+++ routes/api/auth.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+
+// @route GET api/auth
+// @desc Test Route 
+// @access Public 
+
+//we need to return the user info from the token
+router.get('/', auth, async (req, res) => {
+try {
+	const user = await User.findById(req.user.id).select('-password');
+	res.json(user);
+
+ }catch(err){
+console.error(err.message);
+res.status(500).send('Server error');
+}
+
+}); 
+
+//POST request - authentication
+//Logging in with credentials, the token is expected in the response
+router.post('/',
+ [
+ 	check('email', 'Enter email address').isEmail(),
+ 	check('password', 'Password is required').exists()
+	], 
+	async (req, res) => {
+	console.log(req.body);
+	const errors = validationResult(req);
+	if(!errors.isEmpty()){
+		return res.status(400).json({errors: errors.array()});
+	}
+
+	const {email, password } = req.body;
+
+	try{
+
+
+	//See if user exists
+	let user = await User.findOne({email});
+	if(!user) {
+		return res.status(400).json({ errors: [{msg: 'Invalid credentials'}]});
+	}
+
+	//Check if passwords match
+	const isMatch = await bcrypt.compare(password, user.password); //user.password is encrypted here
+
+	if(!isMatch) {
+		
+		return res.status(400).json({ errors: [{msg: 'Invalid credentials'}]});
+	
+	}
+
+	//Return jsonwebtoken
+	const payload = {
+		user: {
+			id: user.id
+		}
+	}
+
+
+
+	jwt.sign(payload, config.get('jwtSecret'), {expiresIn: 360000},
+		(err, token) => {
+			if(err) throw err;
+			res.json({token});
+		});
+
+	//res.send('User Registered');
+	} catch(err) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+
+
+	
+}); 
+
+module.exports = router;
---
+++ routes/api/profile.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+
+
+// @route GET api/profile/me
+// @desc Get current loggedin user's profile
+// @access Private, we will need a token here.  
+
+//We use async-await since we are using mongoose here which always returns a promise
+
+router.get('/me', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+		if(!profile){
+			return res.status(400).json({msg: 'No profile exists for this user'});
+		}
+		res.json(profile);
+	}catch(err){
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+}); 
+
+router.get('/', (req, res) => res.send('POSTS'));
+
+
+// @route POST api/profile 
+// @desc Create or update a user profile
+// @access Private, we will need a token here.
+router.post('/', [auth, [
+	check('status', 'Status is required').not().isEmpty(),
+	check('skills', 'Skills are required').not().isEmpty()
+	]
+	],
+		async (req, res) => {
+			const errors = validationResult(req);
+			if(!errors.isEmpty()){
+				return res.status(400).json({errors: errors.array()});
+			}
+
+			const {company, website, location, bio, status,
+				githubusername, skills, youtube, facebook, twitter, 
+				instagram, linkedin} = req.body;
+
+				//Build profile object
+				const profileFields = {};
+				profileFields.user = req.user.id;
+				if(company) profileFields.company = company;
+				if(website) profileFields.website = website;
+				if(location) profileFields.location = location;
+				if(bio) profileFields.bio = bio;
+				if(status) profileFields.status = status;
+				if(githubusername) profileFields.githubusername = githubusername;
+				if(skills) {
+					profileFields.skills = skills.split(',').map(skill => skill.trim());
+				}
+				
+
+				//Build social object
+				 profileFields.social = {};
+				if(youtube) profileFields.social.youtube = youtube;
+				if(facebook) profileFields.social.facebook = facebook;
+				if(twitter) profileFields.social.twitter = twitter;
+				if(instagram) profileFields.social.instagram = instagram;
+				if(linkedin) profileFields.social.linkedin = linkedin;
+				console.log(profileFields);
+				try{
+					//whenever we use a mongoose method we need to add an await
+					//in front of it as it always returns a promise
+					let profile = await Profile.findOne({user: req.user.id});
+					if(profile){
+						//Update
+						profile = await Profile.findOneAndUpdate({user: req.user.id},
+							{$set: profileFields}, {new: true});
+						return res.json(profile);
+					}
+
+					//Create 
+					profile = new Profile(profileFields);
+					await profile.save();
+					return res.json(profile);
+					
+				}catch(err){
+					console.error(err.message);
+					res.status(500).send('Server Error');
+				}
+					}
+		
+	 );
+
+module.exports = router;
